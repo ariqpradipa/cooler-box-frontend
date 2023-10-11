5336c17d8b2902a932c9c65ff6339435bc7784a9
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-10-11 12:07:33 +0700

    fix: marker position

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -149,7 +149,7 @@ export default function Dashboard() {
                                 url="https://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}"
                                 subdomains={["mt0", "mt1", "mt2", "mt3"]}
                             />
-                            <Marker position={center} icon={point}>
+                            <Marker position={iotLocation} icon={point}>
                                 <Popup>
                                     A popup with information about this location.
                                 </Popup>
